e915db82e88df35c8900d5ac20d5c66fee1c7edd
mswomley <maggieswomley18@gmail.com>
2025-01-03 14:27:19 -0500

    fixed routing

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Routes, Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Routes, Link, BrowserRouter} from "react-router-dom";
 import Home from "./Home"
 import Projects from "./Projects"
 
 function App () {
   return (
-<Router>
-      <nav class="topnav">
-        <Link class="topnav" to="/">Home</Link>
-        <Link class="topnav" to="/projects">Projects</Link>
+<BrowserRouter>
+      <nav className="topnav">
+        <Link className="topnav" to="/">Home</Link>
+        <Link className="topnav" to="/projects">Projects</Link>
       </nav>
   <Routes>
       <Route path="/" element={<Home />} />
@@ -22,7 +22,7 @@ function App () {
       <a href="https://www.linkedin.com/in/maggie-s-5427491a2/"><button className="button" >LinkedIn</button></a><br></br>
       <a href="https://github.com/mswomley"><button className="button" >Github</button></a>
       </footer>
-</Router>
+</BrowserRouter>
   )
 }
 
